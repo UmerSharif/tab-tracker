465d9514628221eeb4285583f40e9f3abee07f52
umer khan <umer.kdev@gmail.com>
2019-02-03 16:16:35 +0200

    server works

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,17 @@
-console.log("hello")
+const express = require("express")
+const bodyParse = require("body-parser")
+const cors = require("cors")
+const morgan = require("morgan")
 
+const app = express()
+app.use(morgan("combined"))
+app.use(bodyParse.json())
+app.use(cors())
+
+app.get("/status", (req, res) => {
+ res.send({
+    message: "helloworld !"
+  })
+})
+
+app.listen(process.env.PORT || 8081)
