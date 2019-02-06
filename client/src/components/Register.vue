4d943a2575cfa54d78c0106ac81234158c2e0bb5
umer khan <umer.kdev@gmail.com>
2019-02-06 19:01:52 +0200

    cleaning up code

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-import AuthenticationService from '../services/AuthenticationService'
+import AuthenticationService from "../services/AuthenticationService";
 export default {
   name: "Register",
   data() {
@@ -21,11 +21,10 @@ export default {
   },
   methods: {
     async register() {
-     const response = await AuthenticationService.register({
-        email:this.email,
-        password:this.password
-      })
-      console.log(response.data)
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      });
     }
   }
 };
